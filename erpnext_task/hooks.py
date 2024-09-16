0c23dd07d28fca42a0eebce2c0b0a85a18275707
Sugesh393 <sugeshgk002@gmail.com>
2024-09-16 12:10:22 +0530

    all tasks

--- erpnext_task/hooks.py
+++ erpnext_task/hooks.py
@@ -227,3 +227,25 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    {
+        "doctype": "Client Script",
+        "filters": [
+            [
+                "name", "in", [
+                    "Party Script", "Appraisee script"
+                ]
+            ]
+        ]
+    },
+    {
+        "doctype": "Notification",
+        "filters": [
+            [
+                "name", "in", [
+                    "Employee retirement Notification", "Retirement Notification"
+                ]
+            ]
+        ]
+    }
+]
